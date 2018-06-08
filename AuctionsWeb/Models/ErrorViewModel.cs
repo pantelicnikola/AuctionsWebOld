c80130d715e7794bdfa103d2efe1fda6348dd273
nikolap <pantelic.n94@gmail.com>
2018-06-08 17:08:06 +0200

    add default VS example code

---
+++ AuctionsWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AuctionsWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
